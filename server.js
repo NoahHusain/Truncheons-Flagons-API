3d56685ef8bf0721661511ab0b78295e5d5f1e24
Noah Husain <noahhusain@gmail.com>
2021-07-31 11:29:18 -0500

    Switiching app to AWS deployment

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('database.json')
-const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 3000
+const path = require('path');
+const dbPath = './api/database.json';
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(dbPath);
+const middlewares = jsonServer.defaults({ static: "./src" });
+const port = process.env.PORT || 5002;
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+
+server.use((req, res, next) => {
+    const isApiRoute = req.originalUrl.includes('/api/');
+    if (isApiRoute) {
+        return next();
+    }
+    return res.sendFile(path.join(__dirname, './src/index.html'));
+});
+
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1'
+}));
+
+server.use(router);
 
 server.listen(port, () => {
-    console.log('JSON Server is running')
-})
+    console.log(`Application running on port ${port}`);
+});
